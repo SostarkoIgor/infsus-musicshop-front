cd78fc4d4cf8d1987cbb92d87cff545513e3b10e
Igor Sostarko <igor.sostarko@fer.hr>
2025-05-23 21:50:25 +0200

    fixing style organization

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,4 @@
 import styles from "../styles/navbar.module.css";	
-import { Link } from "react-router-dom";
-
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,6 +3,6 @@ import { Category } from "../types/category";
 
 let path=import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "http://localhost:8080/api/";
 export const getAllCategories = async (): Promise<Category[]> => {
-    const response = await api.get<Category[]>(path+`categories`);
+    const response = await api.get<Category[]>(path+`categories/all`);
     return response.data;
 }
